9ce739e1d56082d3feebc6ebafa4686bf888b663
negi_gaurav <negigaurav419@gmail.com>
2024-03-05 23:28:05 +0530

    just added a component

--- Food Delivery/src/pages/Homebar.jsx
+++ Food Delivery/src/pages/Homebar.jsx
@@ -13,7 +13,7 @@ export default function Homebar() {
 
         
             <div className="flex-grow mx-4">
-                <Input id="search" type="text" placeholder="Search" className="border-black h-10 w-2/5 " />
+                <Input id="search" type="text" class="placeholder:italic placeholder:text-slate-400 block bg-white w-1/2 border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." />
             </div>
 
         
